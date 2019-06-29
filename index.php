44ebdd413a9341fca318bbfd5773c29ee43fc1b2
Denil Nicolosi <denilnicol@gmail.com>
2019-06-29 15:39:27 +0200

    Aggiunta di comandi

--- index.php
+++ index.php
@@ -19,12 +19,49 @@ if($content) {
         $text = $message['text'];
 		
 		//elaborazione della risposta
-		$message_to_send = "Hai scritto '{$text}'";
+		$message_to_send = elaborateMessage($chat_id, $text);
 		
 		//invio del messaggio di risposta
 		sendMessage($token, $chat_id, $message_to_send);
       
     }
+}
+
+//funzione per elaborare la risposta
+function elaborateMessage($chat_id, $text){
+		
+	switch($text){
+		case "/start":
+			$response="Benvenuto in timezone bot!" + 
+				elaborateMessage($chat_id,"/help");
+			break;
+		case "/help":
+			$response = helpMessage();
+			break;
+		case "/list_timezone":
+			$response="list time zone";
+			break;
+		case "/timezone_from_an_ip":
+			$response="time zone from ip";
+			break;
+		case "/timezone_from_location":
+			$response="timezone from location";
+			break;	
+		case default:
+			$response="Comando non trovato.\n\n" +
+				 elaborateMessage($chat_id,"/help");
+			break;
+	}
+	
+	return $response;
+}
+
+//funzione per stampare messaggio di aiuto
+function helpMessage(){
+	return "Possibili comandi: \n" +
+			"/list_timezone -> stampa la lista di tutte le zone di fuso orario. \n" +
+			"/timezone_from_an_ip -> stampa il fuso orario di una zona dato l'ip. \n" +
+			"/timezone_from_location -> stampa il fuso orario di una zona data. \n"
 }
 
 //funzione per inviare un messaggio di risposta all'utente
