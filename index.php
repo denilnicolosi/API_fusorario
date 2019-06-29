5bed2f269df62a390d01a41011eb6cf8e3ec0476
Denil Nicolosi <denilnicol@gmail.com>
2019-06-29 14:01:08 +0200

    Aggiunta funzione per invio risposta

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
+// php://input restituisce i dati raw (testo), 
+//i dati che si riceveranno saranno in formato Json.
 $content = file_get_contents("php://input");
 
 $token = getenv('BOTTOKEN');
-echo $token;
 
 if($content) {
+	
+	//decodifica del json in array
     $update = json_decode($content, true);
 
     if(isset($update['message'])) {
+		//catturo tutti gli elementi ricevuti da telegram
         $message = $update['message'];
         $message_id = $message['message_id'];
         $chat_id = $message['chat']['id'];
         $from_id = $message['from']['id'];
-
         $text = $message['text'];
+		
+		//elaborazione della risposta
+		$message_to_send = "Hai scritto '{$text}'";
+		
+		//invio del messaggio di risposta
+		sendMessage($chat_id, $message_to_send);
+      
+    }
+}
 
-        $parameters = array(
+//funzione per inviare un messaggio di risposta all'utente
+function sendMessage($chatId, $messageText){
+	
+		$parameters = array(
             'chat_id' => $chat_id,
-            'text' => "You wrote '{$text}'"
+            'text' => $messageText
         );
 
         $url = "https://api.telegram.org/bot{$token}/sendMessage";
@@ -30,5 +45,5 @@ if($content) {
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($handle, CURLOPT_POST, true);
         $response = curl_exec($handle);
-    }
+
 }
