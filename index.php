693c100197edb05a821f91a1a0d05c35013457e5
Denil Nicolosi <denilnicol@gmail.com>
2019-06-30 18:48:00 +0200

    Aggiunta memorizzazione sessione

--- index.php
+++ index.php
@@ -5,7 +5,16 @@ $content = file_get_contents("php://input");
 
 $token = getenv('BOTTOKEN');
 
+//connessione con database
+
+
 if($content) {
+	
+	if(!$conn = pg_connect(getenv("DATABASE_URL"))){
+		error_log("Connessione al database fallita");
+	}else{
+		error_log("Connessione al database riuscita");
+	}
 	
 	//decodifica del json in array
     $update = json_decode($content, true);
@@ -19,7 +28,7 @@ if($content) {
         $text = $message['text'];
 		
 		//elaborazione della risposta
-		$message_to_send = elaborateMessage($chat_id, $text);
+		$message_to_send = elaborateMessage($conn, $chat_id, $text);
 		
 		//invio del messaggio di risposta
 		sendMessage($token, $chat_id, $message_to_send);
@@ -28,32 +37,91 @@ if($content) {
 }
 
 //funzione per elaborare la risposta
-function elaborateMessage($chat_id, $text){
-		
-	switch($text){
-		case "/start":
-			$response="Benvenuto in timezone bot!" .
-				elaborateMessage($chat_id,"/help");
-			break;
-		case "/help":
-			$response = helpMessage();
-			break;
-		case "/list_timezone":
-			$response="list time zone";
-			break;
-		case "/timezone_from_an_ip":
-			$response="time zone from ip";
-			break;
-		case "/timezone_from_location":
-			$response="timezone from location";
-			break;	
-		default:
-			$response="Comando non trovato.\n\n" .
-				 elaborateMessage($chat_id,"/help");
-			break;
-	}
+function elaborateMessage($conn, $chat_id, $text){
 	
+	//stampa di debug della tabella del database	
+	$arr = pg_fetch_all(pg_query($conn, "SELECT * FROM user_id;"));
+	error_log("db: ".print_r($arr));
+	
+	$status=getChatStatus($conn, $chat_id);
+	
+	if(!$status){
+		switch($text){
+			case "/start":
+				$response="Benvenuto in timezone bot!" .
+					elaborateMessage($conn, $chat_id,"/help");
+				break;
+			case "/help":
+				$response = helpMessage();
+				break;
+			case "/list_timezone":
+				$response="list time zone";
+				break;
+			case "/timezone_from_an_ip":
+				$response="Inserire indirizzo ip:";
+				setChatStatus($conn, $chat_id, 1);
+				break;
+			case "/timezone_from_location":
+				$response="timezone from location";
+				setChatStatus($conn, $chat_id, 2);
+				break;	
+			default:
+				$response="Comando non trovato.\n\n" .
+					 elaborateMessage($conn, $chat_id,"/help");
+				break;
+		}
+	}else{
+		switch($status){
+			case 1:
+				$response="ricerca indirizzo {$text}";
+				deleteChat($conn, $chat_id);
+				break;
+			case 2:
+				$response="ricerca località {$text}";
+				deleteChat($conn, $chat_id);
+				break;
+		}
+	}
 	return $response;
+}
+
+//funzione per controllare se esite lo stato della chat e ne ritorna il valore
+function getChatStatus($conn, $chat_id){
+	$res=pg_query($conn, "SELECT status FROM user_id WHERE chat_id={$chat_id};");
+	$arr = pg_fetch_all($res);
+	if($res && pg_num_rows($res)>0){
+		return $arr[0]["status"];
+	}else{
+		return 0;
+	}
+}
+
+//funzione per settare lo stato della chat	
+function setChatStatus($conn, $chat_id, $status){
+    
+    $query="INSERT INTO user_id(chat_id,status)"
+          ." VALUES({$chat_id},{$status})"
+		  ." ON CONFLICT(chat_id) DO"
+		  ." UPDATE"
+          ." SET status = {$status}";
+          
+	$res = pg_query($conn, $query);
+	if (!$res) {
+		error_log("Errore inserimento {$chat_id} ");
+		error_log($query);
+	}
+}
+
+//funzione per eliminare lo stato della chat
+function deleteChat($conn, $chat_id){
+	$query="DELETE FROM user_id "
+          ." WHERE chat_id={$chat_id}";
+		  
+	$res = pg_query($conn, $query);
+	if (!$res) {
+		error_log("Errore cancellazione {$chat_id} ");
+		error_log($query);
+	}
 }
 
 //funzione per stampare messaggio di aiuto
